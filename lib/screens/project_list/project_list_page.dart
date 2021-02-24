75694dcddc4c1df8cc3c0eff66ca9aa21cab2007
ktnydi <kitano.prog@gmail.com>
2021-02-24 19:05:05 +0900

    Add: プロジェクト一覧画面を作成

--- lib/screens/project_list/project_list_page.dart
+++ lib/screens/project_list/project_list_page.dart
@@ -1,11 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:outline_material_icons/outline_material_icons.dart';
 
 class ProjectList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: false,
         title: Text('ProjectHit'),
+        actions: [
+          IconButton(
+            icon: Stack(
+              alignment: Alignment.topRight,
+              children: [
+                Icon(Icons.notifications_outlined),
+                Container(
+                  width: 8,
+                  height: 8,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+            onPressed: () {
+              // TODO: 通知画面を表示
+            },
+          ),
+          IconButton(
+            icon: Icon(OMIcons.settings),
+            onPressed: () {
+              // TODO: 設定画面を表示
+            },
+          ),
+        ],
+      ),
+      body: ListView.separated(
+        padding: EdgeInsets.all(16),
+        separatorBuilder: (context, index) => SizedBox(height: 8),
+        itemBuilder: (context, index) {
+          return Material(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+              side: BorderSide(
+                color: Theme.of(context).dividerColor,
+              ),
+            ),
+            clipBehavior: Clip.antiAlias,
+            child: ListTile(
+              title: Text(
+                'Project Name',
+              ),
+              subtitle: Text('3 Members'),
+              onTap: () {
+                // TODO: タスク一覧画面を表示
+              },
+            ),
+          );
+        },
+        itemCount: 5,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          // TODO: プロジェクト作成画面を表示
+        },
       ),
     );
   }
