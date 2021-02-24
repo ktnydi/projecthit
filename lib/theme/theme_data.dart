6961994dd2bc9a619bce7b2078b93dcc8d2e634d
ktnydi <kitano.prog@gmail.com>
2021-02-24 11:10:35 +0900

    Add: サインイン画面を作成

--- lib/theme/theme_data.dart
+++ lib/theme/theme_data.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-final lightThemeData = ThemeData.from(
+ThemeData lightThemeData = ThemeData.from(
   colorScheme: ColorScheme.light(
     primary: Color(0xFF65AA74),
     onPrimary: Color(0xFFFFFFFF),
@@ -9,9 +9,24 @@ final lightThemeData = ThemeData.from(
   ),
 ).copyWith(
   scaffoldBackgroundColor: Color(0xFFFFFFFF),
+  appBarTheme: AppBarTheme(
+    color: Color(0xFFFFFFFF),
+    brightness: Brightness.light,
+    elevation: 0,
+    iconTheme: IconThemeData(
+      color: Typography.material2014().black.headline6.color,
+    ),
+    textTheme: TextTheme(
+      headline6: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: Typography.material2014().black.headline6.color,
+      ),
+    ),
+  ),
 );
 
-final darkThemeData = ThemeData.from(
+ThemeData darkThemeData = ThemeData.from(
   colorScheme: ColorScheme.dark(
     primary: Color(0xFF65AA74),
     onPrimary: Color(0xFFFFFFFF),
@@ -20,4 +35,19 @@ final darkThemeData = ThemeData.from(
   ),
 ).copyWith(
   scaffoldBackgroundColor: Color(0xFF202020),
+  appBarTheme: AppBarTheme(
+    color: Color(0xFF202020),
+    brightness: Brightness.dark,
+    elevation: 0,
+    iconTheme: IconThemeData(
+      color: Typography.material2014().white.headline6.color,
+    ),
+    textTheme: TextTheme(
+      headline6: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: Typography.material2014().white.headline6.color,
+      ),
+    ),
+  ),
 );
