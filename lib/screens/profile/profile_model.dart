f94b5666d2b1c47a56152c292d81d023c8a8d903
ktnydi <kitano.prog@gmail.com>
2021-03-03 16:57:05 +0900

    Tweak Fix: プロフィール画面を修正

--- lib/screens/profile/profile_model.dart
+++ lib/screens/profile/profile_model.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ProfileModel extends ChangeNotifier {
-  final name = TextEditingController();
-  final about = TextEditingController();
-  bool isEditingName = false;
-  bool isEditingAbout = false;
   bool isLoading = false;
 
   void beginLoading() {
@@ -17,10 +13,6 @@ class ProfileModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  void reload() {
-    notifyListeners();
-  }
-
   Future<void> updateName(String name) async {
     // TODO: 名前の更新
     await Future.delayed(Duration(milliseconds: 3000));
@@ -29,12 +21,5 @@ class ProfileModel extends ChangeNotifier {
   Future<void> updateAbout(String about) async {
     // TODO: 自己紹介の更新
     await Future.delayed(Duration(milliseconds: 3000));
-  }
-
-  @override
-  void dispose() {
-    name.dispose();
-    about.dispose();
-    super.dispose();
   }
 }
--- lib/screens/profile/profile_page.dart
+++ lib/screens/profile/profile_page.dart
@@ -3,30 +3,27 @@ import 'package:projecthit/screens/profile/profile_model.dart';
 import 'package:provider/provider.dart';
 
 class Profile extends StatelessWidget {
-  void _editName(ProfileModel profileModel) {
-    // 自己紹介が編集中なら名前は編集できない
-    if (profileModel.isEditingAbout) return;
-
-    profileModel.isEditingName = true;
-    profileModel.reload();
-  }
+  final _nameKey = GlobalKey<FormFieldState<String>>();
+  final _aboutKey = GlobalKey<FormFieldState<String>>();
+  final name = 'Yudai Kitano';
+  final about = '';
 
   Future<void> _updateName(
     BuildContext context,
     ProfileModel profileModel,
-    String newName,
   ) async {
+    if (!_nameKey.currentState.validate()) return;
+
+    FocusScope.of(context).unfocus();
+
+    final newName = _nameKey.currentState.value;
+
+    if (name == newName) return;
+
     try {
-      FocusScope.of(context).unfocus();
-
-      if (newName.trim().isNotEmpty) {
-        profileModel.beginLoading();
-        await profileModel.updateName(newName);
-        profileModel.endLoading();
-      }
-
-      profileModel.isEditingName = false;
-      profileModel.reload();
+      profileModel.beginLoading();
+      await profileModel.updateName(newName);
+      profileModel.endLoading();
     } catch (e) {
       profileModel.endLoading();
       showDialog(
@@ -49,30 +46,22 @@ class Profile extends StatelessWidget {
     }
   }
 
-  void _editAbout(ProfileModel profileModel) {
-    // 名前が編集中なら自己紹介は編集できない
-    if (profileModel.isEditingName) return;
-
-    profileModel.isEditingAbout = true;
-    profileModel.reload();
-  }
-
   Future<void> _updateAbout(
     BuildContext context,
     ProfileModel profileModel,
-    String newAbout,
   ) async {
+    if (!_aboutKey.currentState.validate()) return;
+
+    FocusScope.of(context).unfocus();
+
+    final newAbout = _aboutKey.currentState.value;
+
+    if (newAbout == about) return;
+
     try {
-      FocusScope.of(context).unfocus();
-
-      if (newAbout.trim().isNotEmpty) {
-        profileModel.beginLoading();
-        await profileModel.updateAbout(newAbout);
-        profileModel.endLoading();
-      }
-
-      profileModel.isEditingAbout = false;
-      profileModel.reload();
+      profileModel.beginLoading();
+      await profileModel.updateAbout(newAbout);
+      profileModel.endLoading();
     } catch (e) {
       profileModel.endLoading();
       showDialog(
@@ -97,9 +86,6 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String name = 'Yudai Kitano';
-    String about = '';
-
     return ChangeNotifierProvider<ProfileModel>(
       create: (_) => ProfileModel(),
       builder: (context, child) {
@@ -110,6 +96,14 @@ class Profile extends StatelessWidget {
             Scaffold(
               appBar: AppBar(
                 title: Text('Profile'),
+                actions: [
+                  IconButton(
+                    icon: Icon(Icons.logout),
+                    onPressed: () {
+                      // TODO: サインアウトする
+                    },
+                  ),
+                ],
               ),
               body: SingleChildScrollView(
                 padding: EdgeInsets.all(16),
@@ -160,179 +154,72 @@ class Profile extends StatelessWidget {
                       Text(
                         'Name',
                         style: TextStyle(
-                          color: Theme.of(context).colorScheme.secondary,
                           fontSize: 18,
                         ),
                       ),
                       SizedBox(height: 4),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: context.select(
-                                    (ProfileModel model) => model.isEditingName)
-                                ? Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 16),
-                                    child: TextFormField(
-                                      validator: (value) {
-                                        if (value.trim().isEmpty) {
-                                          return 'Enter Name';
-                                        }
-
-                                        if (value.length > 50) {
-                                          return 'Name is too long';
-                                        }
-
-                                        return null;
-                                      },
-                                      autovalidateMode: AutovalidateMode.always,
-                                      controller: profileModel.name
-                                        ..text = name
-                                        ..selection =
-                                            TextSelection.fromPosition(
-                                          TextPosition(
-                                            offset: name.length,
-                                          ),
-                                        ),
-                                      autofocus: true,
-                                      decoration: InputDecoration(
-                                        contentPadding: EdgeInsets.zero,
-                                        isDense: true,
-                                        border: InputBorder.none,
-                                      ),
-                                      onChanged: (value) {
-                                        name = value;
-                                      },
-                                      onFieldSubmitted: (value) async {
-                                        await _updateName(
-                                          context,
-                                          profileModel,
-                                          value,
-                                        );
-                                      },
-                                    ),
-                                  )
-                                : Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 16),
-                                    child: Text(
-                                      '$name',
-                                      style:
-                                          Theme.of(context).textTheme.subtitle1,
-                                    ),
-                                  ),
-                          ),
-                          IconButton(
-                            icon: context.select(
-                              (ProfileModel model) => model.isEditingName,
-                            )
-                                ? Icon(Icons.done)
-                                : Icon(Icons.edit_outlined),
-                            visualDensity: VisualDensity.compact,
-                            onPressed: () async {
-                              if (!profileModel.isEditingName) {
-                                return _editName(profileModel);
+                      Builder(
+                        builder: (context) {
+                          return TextFormField(
+                            key: _nameKey,
+                            initialValue: name,
+                            validator: (value) {
+                              if (value.trim().isEmpty) {
+                                return 'Enter Name';
                               }
 
+                              if (value.length > 50) {
+                                return 'Name is too long';
+                              }
+
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                            ),
+                            onFieldSubmitted: (value) async {
                               await _updateName(
                                 context,
                                 profileModel,
-                                name,
                               );
                             },
-                          ),
-                        ],
+                          );
+                        },
                       ),
                       SizedBox(height: 16),
                       Text(
                         'About',
                         style: TextStyle(
-                          color: Theme.of(context).colorScheme.secondary,
                           fontSize: 18,
                         ),
                       ),
                       SizedBox(height: 4),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: context.select((ProfileModel model) =>
-                                    model.isEditingAbout)
-                                ? Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 16),
-                                    child: TextFormField(
-                                      validator: (value) {
-                                        if (1 <= value.length &&
-                                            value.length < 6) {
-                                          return 'About is too short';
-                                        }
+                      TextFormField(
+                        key: _aboutKey,
+                        initialValue: about,
+                        validator: (value) {
+                          if (1 <= value.length && value.length < 6) {
+                            return 'About is too short';
+                          }
 
-                                        if (value.length > 170) {
-                                          return 'About is too long';
-                                        }
+                          if (value.length > 170) {
+                            return 'About is too long';
+                          }
 
-                                        return null;
-                                      },
-                                      autovalidateMode: AutovalidateMode.always,
-                                      controller: profileModel.about
-                                        ..text = about
-                                        ..selection =
-                                            TextSelection.fromPosition(
-                                          TextPosition(
-                                            offset: about.length,
-                                          ),
-                                        ),
-                                      autofocus: true,
-                                      maxLines: null,
-                                      keyboardType: TextInputType.text,
-                                      decoration: InputDecoration(
-                                        contentPadding: EdgeInsets.zero,
-                                        isDense: true,
-                                        border: InputBorder.none,
-                                        hintText:
-                                            'Introduce yourself to member',
-                                      ),
-                                      onChanged: (value) {
-                                        about = value;
-                                      },
-                                      onFieldSubmitted: (value) async {
-                                        await _updateAbout(
-                                          context,
-                                          profileModel,
-                                          value,
-                                        );
-                                      },
-                                    ),
-                                  )
-                                : Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 16),
-                                    child: Text(
-                                      '$about',
-                                      style:
-                                          Theme.of(context).textTheme.subtitle1,
-                                    ),
-                                  ),
-                          ),
-                          IconButton(
-                            icon: context.select((ProfileModel model) =>
-                                    model.isEditingAbout)
-                                ? Icon(Icons.done)
-                                : Icon(Icons.edit_outlined),
-                            visualDensity: VisualDensity.compact,
-                            onPressed: () async {
-                              if (!profileModel.isEditingAbout) {
-                                return _editAbout(profileModel);
-                              }
-
-                              await _updateAbout(
-                                context,
-                                profileModel,
-                                about,
-                              );
-                            },
-                          ),
-                        ],
+                          return null;
+                        },
+                        maxLines: 5,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Introduce yourself to member',
+                        ),
+                        onFieldSubmitted: (value) async {
+                          await _updateAbout(
+                            context,
+                            profileModel,
+                          );
+                        },
                       ),
                     ],
                   ),
