6937ead0e361f6f22e737e3b7613f203b8b386a0
ktnydi <kitano.prog@gmail.com>
2021-03-15 01:02:04 +0900

    Add: 完了したタスクの一括削除機能を作成

--- lib/screens/task_list/task_list_model.dart
+++ lib/screens/task_list/task_list_model.dart
@@ -6,8 +6,19 @@ import 'package:projecthit/repository/task_repository.dart';
 class TaskListModel extends ChangeNotifier {
   final Project project;
   final _taskRepository = TaskRepository();
+  bool isLoading = false;
 
   TaskListModel({@required this.project});
+
+  void beginLoading() {
+    isLoading = true;
+    notifyListeners();
+  }
+
+  void endLoading() {
+    isLoading = false;
+    notifyListeners();
+  }
 
   Stream<List<Task>> fetchTasks() {
     return _taskRepository.fetchTasks(project);
@@ -15,5 +26,9 @@ class TaskListModel extends ChangeNotifier {
 
   Future<void> doneTask(Task task) async {
     await _taskRepository.doneTask(project, task);
+  }
+
+  Future<void> deleteDoneTask() async {
+    await _taskRepository.deleteDoneTasks(project);
   }
 }
