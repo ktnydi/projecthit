2959a62b5bd26a303cd5e4de9dadc2816e14ec09
ktnydi <kitano.prog@gmail.com>
2021-03-18 15:21:22 +0900

    Add: DynamicLinkを受信する機能を作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:package_info/package_info.dart';
+import 'package:projecthit/screens/accept_invitation/accept_invitation_page.dart';
 import 'package:projecthit/screens/my_app/my_app_page.dart';
 import 'package:provider/provider.dart';
 
@@ -8,14 +10,29 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
+  final navigatorKey = GlobalKey<NavigatorState>();
   final packageInfo = await PackageInfo.fromPlatform();
+
+  // 未インストールのとき
+  final data = await FirebaseDynamicLinks.instance.getInitialLink();
+  final deepLink = data?.link;
+  if (deepLink != null) {
+    navigatorKey.currentState.push(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (context) => AcceptInvitation(
+          deepLink: deepLink,
+        ),
+      ),
+    );
+  }
 
   runApp(
     MultiProvider(
       providers: [
         Provider<PackageInfo>.value(value: packageInfo),
       ],
-      child: MyApp(),
+      child: MyApp(navigatorKey: navigatorKey),
     ),
   );
 }
--- lib/screens/my_app/my_app_page.dart
+++ lib/screens/my_app/my_app_page.dart
@@ -1,11 +1,49 @@
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
+import 'package:projecthit/screens/accept_invitation/accept_invitation_page.dart';
 import 'package:projecthit/screens/auth/auth_page.dart';
 import 'package:projecthit/screens/my_app/my_app_model.dart';
 import 'package:projecthit/screens/project_list/project_list_page.dart';
 import 'package:projecthit/theme/theme_data.dart';
 import 'package:provider/provider.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  MyApp({@required this.navigatorKey});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    // インストール済みのとき
+    FirebaseDynamicLinks.instance.onLink(
+      onSuccess: (PendingDynamicLinkData dynamicLink) async {
+        final deepLink = dynamicLink?.link;
+        if (deepLink != null) {
+          widget.navigatorKey.currentState.pushAndRemoveUntil(
+            MaterialPageRoute(
+              fullscreenDialog: true,
+              builder: (context) => AcceptInvitation(
+                deepLink: deepLink,
+              ),
+            ),
+            (route) => route.isFirst,
+          );
+        }
+      },
+      onError: (OnLinkErrorException e) async {
+        print('onLinkError');
+        print(e.message);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<MyAppModel>(
@@ -15,6 +53,7 @@ class MyApp extends StatelessWidget {
 
         return MaterialApp(
           title: 'Flutter Demo',
+          navigatorKey: widget.navigatorKey,
           theme: lightThemeData,
           darkTheme: darkThemeData,
           home: myAppModel.currentUser != null ? ProjectList() : Auth(),
