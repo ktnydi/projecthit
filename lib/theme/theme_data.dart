d5240c87af0d3c7da070d40dc8e8ab06e341c119
ktnydi <kitano.prog@gmail.com>
2021-03-23 12:06:51 +0900

    Fix: アプリテーマを改善

--- lib/theme/theme_data.dart
+++ lib/theme/theme_data.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
+const mainColor = Color(0xFF54B2E4);
+const onMainColor = Color(0xFFFFFFFF);
+const scaffoldBackgroundLightColor = Color(0xFFE6F4FB);
+const scaffoldBackgroundDarkColor = Color(0xFF303030);
+const dividerLightColor = Color(0x1F000000);
+const dividerDarkColor = Color(0x1FFFFFFF);
+const backgroundLightColor = Color(0xFFFFFFFF);
+const backgroundDarkColor = Color(0xFF404040);
+
 ThemeData lightThemeData = ThemeData.from(
   colorScheme: ColorScheme.light(
-    primary: Color(0xFF29B397),
-    onPrimary: Color(0xFFFFFFFF),
-    secondary: Color(0xFF29B397),
-    onSecondary: Color(0xFFFFFFFF),
+    primary: mainColor,
+    onPrimary: onMainColor,
+    secondary: mainColor,
+    onSecondary: onMainColor,
   ),
   textTheme: TextTheme(
     subtitle1: TextStyle(
@@ -13,10 +22,11 @@ ThemeData lightThemeData = ThemeData.from(
     ),
   ),
 ).copyWith(
-  scaffoldBackgroundColor: Color(0xFFFFFFFF),
-  canvasColor: Color(0xFFFFFFFF),
+  scaffoldBackgroundColor: scaffoldBackgroundLightColor,
+  canvasColor: backgroundLightColor,
+  splashColor: Colors.transparent,
   appBarTheme: AppBarTheme(
-    color: Color(0xFFFFFFFF),
+    color: backgroundLightColor,
     brightness: Brightness.light,
     elevation: 0,
     iconTheme: IconThemeData(
@@ -57,14 +67,33 @@ ThemeData lightThemeData = ThemeData.from(
       borderRadius: BorderRadius.circular(8),
     ),
   ),
+  inputDecorationTheme: InputDecorationTheme(
+    fillColor: backgroundLightColor,
+    filled: true,
+    border: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: dividerLightColor,
+      ),
+    ),
+    disabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: dividerLightColor,
+      ),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: dividerLightColor,
+      ),
+    ),
+  ),
 );
 
 ThemeData darkThemeData = ThemeData.from(
   colorScheme: ColorScheme.dark(
-    primary: Color(0xFF29B397),
-    onPrimary: Color(0xFFFFFFFF),
-    secondary: Color(0xFF29B397),
-    onSecondary: Color(0xFFFFFFFF),
+    primary: mainColor,
+    onPrimary: onMainColor,
+    secondary: mainColor,
+    onSecondary: onMainColor,
   ),
   textTheme: TextTheme(
     subtitle1: TextStyle(
@@ -72,8 +101,9 @@ ThemeData darkThemeData = ThemeData.from(
     ),
   ),
 ).copyWith(
-  scaffoldBackgroundColor: Color(0xFF202020),
-  canvasColor: Color(0xFF202020),
+  scaffoldBackgroundColor: scaffoldBackgroundDarkColor,
+  canvasColor: backgroundDarkColor,
+  splashColor: Colors.transparent,
   appBarTheme: AppBarTheme(
     color: Color(0xFF202020),
     brightness: Brightness.dark,
@@ -114,6 +144,25 @@ ThemeData darkThemeData = ThemeData.from(
   dialogTheme: DialogTheme(
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(8),
+    ),
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    fillColor: backgroundDarkColor,
+    filled: true,
+    border: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: dividerDarkColor,
+      ),
+    ),
+    disabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: dividerDarkColor,
+      ),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: dividerDarkColor,
+      ),
     ),
   ),
 );
