b548be8f1cc60f9b98ef7790746f2424461cfe73
ktnydi <kitano.prog@gmail.com>
2021-03-27 15:15:46 +0900

    Fix: Userドキュメントに所属プロジェクト数カウント用フィールドを追加
    フィールド名をスネークケースからキャメルケースに修正

--- lib/entity/app_user.dart
+++ lib/entity/app_user.dart
@@ -1,34 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:english_words/english_words.dart';
+import 'package:projecthit/entity/app_user_field.dart';
 
 class AppUser {
   String id;
   String name;
   String icon;
   String about;
+  int sumUserProjects;
   Timestamp createdAt;
   Timestamp updatedAt;
 
   AppUser({this.id});
 
   AppUser.fromMap(Map<String, dynamic> map) {
-    id = map['id'];
-    name = map['name'];
-    icon = map['icon'];
-    about = map['about'];
-    createdAt = map['created_at'] as Timestamp;
-    updatedAt = map['updated_at'] as Timestamp;
+    id = map[AppUserField.id];
+    name = map[AppUserField.name];
+    icon = map[AppUserField.iconUrl];
+    about = map[AppUserField.about];
+    sumUserProjects = map[AppUserField.sumUserProjects];
+    createdAt = map[AppUserField.createdAt] as Timestamp;
+    updatedAt = map[AppUserField.updatedAt] as Timestamp;
   }
 
   // DB保存用
   Map<String, dynamic> toMap() {
     return {
-      'id': id,
-      'name': name ?? WordPair.random().asPascalCase,
-      'icon': icon,
-      'about': about ?? '',
-      'created_at': createdAt ?? FieldValue.serverTimestamp(),
-      'updated_at': FieldValue.serverTimestamp(),
+      AppUserField.id: id,
+      AppUserField.name: name ?? WordPair.random().asPascalCase,
+      AppUserField.iconUrl: icon,
+      AppUserField.about: about ?? '',
+      AppUserField.sumUserProjects: sumUserProjects ?? 0,
+      AppUserField.createdAt: createdAt ?? FieldValue.serverTimestamp(),
+      AppUserField.updatedAt: FieldValue.serverTimestamp(),
     };
   }
 }
---
+++ lib/entity/app_user_field.dart
@@ -0,0 +1,9 @@
+class AppUserField {
+  static const id = 'id';
+  static const name = 'name';
+  static const about = 'about';
+  static const iconUrl = 'iconUrl';
+  static const sumUserProjects = 'sumUserProjects';
+  static const updatedAt = 'updatedAt';
+  static const createdAt = 'createdAt';
+}
