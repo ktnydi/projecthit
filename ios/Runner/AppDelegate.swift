20579ebbb020b95d1bccbd9b2e759e38d02f543a
ktnydi <kitano.prog@gmail.com>
2021-03-29 16:18:26 +0900

    Add: 起動時にバッジを削除するメソッドを追加

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,4 +10,8 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  override func applicationDidBecomeActive(_ application: UIApplication) {
+    UIApplication.shared.applicationIconBadgeNumber = 0
+  }
 }
