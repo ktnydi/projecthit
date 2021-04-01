01710cb44c12b7276a0cd48b960b49411a928ffd
ktnydi <kitano.prog@gmail.com>
2021-04-01 15:51:42 +0900

    Add: l10nの対応準備

--- lib/screens/my_app/my_app_page.dart
+++ lib/screens/my_app/my_app_page.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:projecthit/model/theme_model.dart';
 import 'package:projecthit/screens/accept_invitation/accept_invitation_page.dart';
 import 'package:projecthit/screens/auth/auth_page.dart';
@@ -54,6 +56,16 @@ class _MyAppState extends State<MyApp> {
 
         return MaterialApp(
           title: 'Flutter Demo',
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: [
+            const Locale('en', ''),
+            const Locale('ja', ''),
+          ],
           navigatorKey: widget.navigatorKey,
           theme: context.select((ThemeModel model) => model.isDarkMode)
               ? darkThemeData
