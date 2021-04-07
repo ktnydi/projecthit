5a31fefdfeb1628b5609281310d6ecb92e6630fe
ktnydi <kitano.prog@gmail.com>
2021-04-07 15:13:14 +0900

    Fix: プロジェクト詳細画面への画面遷移を修正

--- lib/screens/task_list/task_list_page.dart
+++ lib/screens/task_list/task_list_page.dart
@@ -114,6 +114,7 @@ class _TaskListState extends State<TaskList> {
                     onPressed: () => Navigator.push(
                       context,
                       MaterialPageRoute(
+                        fullscreenDialog: true,
                         builder: (context) =>
                             ProjectDetail(project: widget.project),
                       ),
