e1c85516dfff1e85537af79970e6d3011aeb732d
ktnydi <kitano.prog@gmail.com>
2021-04-08 17:09:57 +0900

    Fix: アプリ設定をローカルに保存するよう変更

--- lib/model/theme_model.dart
+++ lib/model/theme_model.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:projecthit/entity/app_setting.dart';
 import 'package:projecthit/enum/appearance.dart';
 import 'package:projecthit/repository/setting_repository.dart';
-import 'package:projecthit/repository/user_repository.dart';
 
 class ThemeModel extends ChangeNotifier {
   final _settingRepository = SettingRepository();
-  final _userRepository = UserRepository();
   Appearance appearance = Appearance.light;
 
   bool get isDarkMode => appearance == Appearance.dark;
@@ -16,8 +14,6 @@ class ThemeModel extends ChangeNotifier {
   }
 
   Future<void> fetchTheme() async {
-    if (_userRepository.currentUser == null) return;
-
     final appSetting = await _settingRepository.fetchAppSetting();
     appearance = appSetting.isDark ? Appearance.dark : Appearance.light;
     notifyListeners();
